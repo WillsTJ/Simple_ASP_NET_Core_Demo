12320b52e4f7de35cea4322cf95d7f27329d61cd
willi <willi@DESKTOP-MDVVF0K>
2023-05-15 06:07:42 +0100

    Add project files.

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            /*DogViewModel dogModel = new DogViewModel()
+            {
+                Name = "Seth",
+                Age = 2
+            };*/
+
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            // return View("Index");
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
